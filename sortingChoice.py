ddce9f0537c0370ac88a04de438ca0a2720ca937
Sergey Zholudev <serg.zholudev@gmail.com>
2024-05-21 20:07:58 +0300

    The code of sortingChoice(сортировка выбором) закончен, протестирован, работает.

--- sortingChoice.py
+++ sortingChoice.py
@@ -4,6 +4,7 @@
 import random
 
 def findSmallest(arr):
+    """Функция выбора наименьшего элемента из списка"""
     smallest = arr[0]
     smallest_index = 0
     for i in range(len(arr)):
@@ -12,7 +13,18 @@ def findSmallest(arr):
             smallest_index = i
     return smallest_index
 
+def sortingChoice(arr):
+    """Функция сортировки списка выбором. О(n2)"""
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
 
 # создаём список вещественных чисел
 numbers = [random.randint(0, 100) for i in range(0, 15)]
 print(f"Несортированный список \n  {numbers}")
+
+# основное тело 
+sortedArr = sortingChoice(numbers)
+print(f"Сортированный массив \n  {sortedArr}")
