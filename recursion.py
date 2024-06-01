66ed40d2c5d42b805e28551487ef8b17f30ecb62
Sergey Zholudev <serg.zholudev@gmail.com>
2024-06-01 23:52:36 +0300

    Активированы все примеры рекурсивных функций. JSON файл конфигурации настроен под Python.

--- recursion.py
+++ recursion.py
@@ -1,20 +1,20 @@
 # демонстрация работы рекурсивной функции
 
-# def sum(n):
-#     if n == 0:
-#         return 0
-#     else:
-#         return n + sum(n - 1)
+def sum(n):
+    if n == 0:
+        return 0
+    else:
+        return n + sum(n - 1)
 
-# print(sum(10))
+print(sum(10))
 
-# def fact(x):
-#     if x == 1:
-#         return 1
-#     else:
-#         return x * fact(x - 1)
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x - 1)
     
-# print(fact(5))
+print(fact(5))
 
 def count(arr):
     if not arr:
@@ -22,5 +22,5 @@ def count(arr):
     else:
         return 1 + count(arr[1:])
 
-numbers = [i for i in range(30)]    
+numbers = [i for i in range(8)]    
 print(count(numbers))
